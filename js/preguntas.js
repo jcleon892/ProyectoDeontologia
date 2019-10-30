10c902cad083b2c603bc9ba0cb1d2fd6ec7020f3
jcleon892 <jleon267@unab.edu.co>
2019-10-29 20:16:20 -0500

    jcl
    control

--- js/preguntas.js
+++ js/preguntas.js
@@ -7,92 +7,10 @@ function pregunta(){
 	
 
 	initGame=false;
-
-	
-
-
-    var centrotx =window.widthArea/2;
-    console.log(centrotx);
-   preguntatxt = document.createElement('div');
-   preguntatxt.style.position = 'absolute';
-   preguntatxt.style.width = 100;
-   preguntatxt.style.height = 100;
-   preguntatxt.innerHTML = "pregunta pep";
-   preguntatxt.style.top = 425 + 'px';   
-   preguntatxt.style.left = centrotx + 'px';
-   //preguntatxt.style.alignItems="center";
-   preguntatxt.style.font = "40px MyEpistolar";
-   preguntatxt.style.color = "white";
-   document.body.appendChild(preguntatxt);
-
-
-
-   var optA;
-   optA = document.createElement('div');
-   optA.style.position = 'absolute';
-   optA.style.width = 100;
-   optA.style.height = 100;
-   optA.innerHTML = "Respuesta A";
-   optA.style.top =550 + 'px';   
-   optA.style.left = 150 + 'px';
-   optA.style.font = "40px MyEpistolar";
-   optA.style.color = "white";
-   document.body.appendChild(optA);
-
-   var optB;
-   optB = document.createElement('div');
-   optB.style.position = 'absolute';
-   optB.style.width = 100;
-   optB.style.height = 100;
-   optB.innerHTML = "Respuesta B";
-   optB.style.top = 550 + 'px';   
-   optB.style.left = 700 + 'px';
-   optB.style.font = "40px MyEpistolar";
-   optB.style.color = "white";
-   document.body.appendChild(optB);
-
-   var optC;
-   optC = document.createElement('div');
-   optC.style.position = 'absolute';
-   optC.style.width = 100;
-   optC.style.height = 100;
-   optC.innerHTML = "Respuesta C";
-   optC.style.top = 650 + 'px';   
-   optC.style.left = 150 + 'px';
-   optC.style.font = "40px MyEpistolar";
-   optC.style.color = "white";
-   document.body.appendChild(optC);
-
-   var optD;
-   optD = document.createElement('div');
-   optD.style.position = 'absolute';
-   optD.style.width = 100;
-   optD.style.height = 100;
-   optD.innerHTML = "Respuesta D";
-   optD.style.top = 650 + 'px';   
-   optD.style.left = 700 + 'px';
-   optD.style.font = "40px MyEpistolar";
-   optD.style.color = "white";
-   document.body.appendChild(optD);
-
-
-
-	document.getElementById("Preg").disabled = false;
-	document.getElementById("Preg").style.display = "block";
-		
-	
-	
-
-
-    //document.getElementById("btnA").style.position = 'absolute';
-  
-
-  //  document.getElementById("Preg").innerHTML ="hola";
-
-     
-
-   
-  
+ 
+  document.getElementById("Preg").disabled = false;
+  document.getElementById("Preg").style.display = "block";
+  document.getElementById("imgPre").src="Images/quest/1.1.jpeg";
     
 	document.getElementById("btnA").addEventListener('click', function (event)
 	{
@@ -113,7 +31,7 @@ function pregunta(){
 	{
 		console.log("boton D")
 	});
-	//document.body.appendChild(pregunta);
+
 
 	
 
